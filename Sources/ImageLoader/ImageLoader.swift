6e903a4f25149866eb57d89b3e19c2f9ebd4576e
Artur Fortunato <artur.rf@hotmail.com>
2025-09-26 22:25:52 -0300

    thread limit

--- Sources/ImageLoader/ImageLoader.swift
+++ Sources/ImageLoader/ImageLoader.swift
@@ -1,40 +1,99 @@
 import UIKit
 
 public protocol ImageLoading {
-    func loadImage(from urlString: String, completion: @escaping (Bool) -> Void)
+    @discardableResult
+    func loadImage(from urlString: String, completion: @escaping (UIImage?) -> Void) -> UUID?
+    func cancelLoad(_ token: UUID)
 }
 
 public final class ImageLoader: ImageLoading {
     public static let shared = ImageLoader()
+    
     private let cache = NSCache<NSString, UIImage>()
+    private let queue = DispatchQueue(label: "com.imageloader.queue")
+
+    private var pendingRequests = [String: [(UUID, (UIImage?) -> Void)]]()
+    
+    private var tokenToURL = [UUID: String]()
+    
+    private let downloadQueue: OperationQueue = {
+        let queue = OperationQueue()
+        queue.name = "com.imageloader.downloadQueue"
+        queue.maxConcurrentOperationCount = 4
+        return queue
+    }()
     
     private init() {}
     
-    public func loadImage(from urlString: String, completion: @escaping (Bool) -> Void) {
-        if let _ = cache.object(forKey: urlString as NSString) {
-            DispatchQueue.main.async {
-                completion(true)
-            }
-            return
+    @discardableResult
+    public func loadImage(from urlString: String, completion: @escaping (UIImage?) -> Void) -> UUID? {
+        
+        if let cached = cache.object(forKey: urlString as NSString) {
+            DispatchQueue.main.async { completion(cached) }
+            return nil
         }
         
         guard let url = URL(string: urlString) else {
-            DispatchQueue.main.async {
-                completion(false)
-            }
-            return
+            DispatchQueue.main.async { completion(nil) }
+            return nil
         }
         
-        URLSession.shared.dataTask(with: url) { [weak self] data, _, _ in
-            var sucesso = false
-            if let data, let image = UIImage(data: data) {
-                self?.cache.setObject(image, forKey: urlString as NSString)
-                sucesso = true
+        let token = UUID()
+        
+        queue.sync {
+            if pendingRequests[urlString] != nil {
+                pendingRequests[urlString]?.append((token, completion))
+                tokenToURL[token] = urlString
+                return
+            } else {
+                pendingRequests[urlString] = [(token, completion)]
+                tokenToURL[token] = urlString
+            }
+        }
+        
+        let operation = BlockOperation { [weak self] in
+            guard let self = self else { return }
+            
+            let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+                var image: UIImage? = nil
+                if let data, let downloaded = UIImage(data: data) {
+                    image = downloaded
+                    self.cache.setObject(downloaded, forKey: urlString as NSString)
+                }
+                
+                self.queue.sync {
+                    let completions = self.pendingRequests[urlString] ?? []
+                    self.pendingRequests[urlString] = nil
+                    completions.forEach { (_, completion) in
+                        DispatchQueue.main.async { completion(image) }
+                    }
+                    completions.forEach { (token, _) in
+                        self.tokenToURL[token] = nil
+                    }
+                }
             }
             
-            DispatchQueue.main.async {
-                completion(sucesso)
+            task.resume()
+            
+            self.queue.sync {
+                self.tokenToURL[token] = urlString
             }
-        }.resume()
+        }
+        
+        downloadQueue.addOperation(operation)
+        
+        return token
+    }
+    
+    public func cancelLoad(_ token: UUID) {
+        queue.sync {
+            guard let urlString = tokenToURL[token] else { return }
+            if var callbacks = pendingRequests[urlString] {
+                callbacks.removeAll { $0.0 == token }
+                pendingRequests[urlString] = callbacks.isEmpty ? nil : callbacks
+            }
+            
+            tokenToURL[token] = nil
+        }
     }
 }
--- Tests/ImageLoaderTests/ImageLoaderTests.swift
+++ Tests/ImageLoaderTests/ImageLoaderTests.swift
@@ -1,22 +1,22 @@
 import Testing
-
 @testable import ImageLoader
 
 @Suite("Image Loader")
 @MainActor
 struct ImageLoaderTests {
-    @Test("Returns false for invalid URL")
+    
+    @Test("Returns nil for invalid URL")
     @available(iOS 13.0, *)
     func testInvalidURL() async {
         let loader = ImageLoader.shared
         
-        let didSet = await withCheckedContinuation { continuation in
-            loader.loadImage(from: "invalid-url") { success in
-                continuation.resume(returning: success)
+        let image = await withCheckedContinuation { continuation in
+            loader.loadImage(from: "invalid-url") { img in
+                continuation.resume(returning: img)
             }
         }
         
-        #expect(didSet == false)
+        #expect(image == nil)
     }
     
     @Test("Successfully downloads a valid image")
@@ -24,33 +24,50 @@ struct ImageLoaderTests {
     func testValidImageDownload() async {
         let loader = ImageLoader.shared
         
-        let didSet = await withCheckedContinuation { continuation in
-            loader.loadImage(from: "https://picsum.photos/50") { success in
-                continuation.resume(returning: success)
+        let image = await withCheckedContinuation { continuation in
+            loader.loadImage(from: "https://picsum.photos/50") { img in
+                continuation.resume(returning: img)
             }
         }
         
-        #expect(didSet == true)
+        #expect(image != nil)
     }
     
-    @Test("Returns true for cached image on second load")
+    @Test("Returns cached image on second load")
     @available(iOS 13.0, *)
     func testCacheHit() async {
         let loader = ImageLoader.shared
         let url = "https://picsum.photos/60"
         
         _ = await withCheckedContinuation { continuation in
-            loader.loadImage(from: url) { success in
-                continuation.resume(returning: success)
+            loader.loadImage(from: url) { img in
+                continuation.resume(returning: img)
             }
         }
         
-        let didSetCache = await withCheckedContinuation { continuation in
-            loader.loadImage(from: url) { success in
-                continuation.resume(returning: success)
+        let cachedImage = await withCheckedContinuation { continuation in
+            loader.loadImage(from: url) { img in
+                continuation.resume(returning: img)
             }
         }
         
-        #expect(didSetCache == true)
+        #expect(cachedImage != nil)
+    }
+    
+    @Test("Allows canceling a download")
+    @available(iOS 13.0, *)
+    func testCancelDownload() async {
+        let loader = ImageLoader.shared
+        let url = "https://picsum.photos/200"
+        
+        let token = loader.loadImage(from: url) { img in
+            #expect(false)
+        }
+        
+        loader.cancelLoad(token!)
+        
+        try? await Task.sleep(nanoseconds: 1_000_000_000)
+        
+        #expect(true)
     }
 }
